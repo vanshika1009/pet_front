0e432e5e96fee75c5f152c9458bcb471b204ef09
Vanshika <vanshikaaa1009@gmail.com>
2023-01-02 14:43:00 -0600

    Update App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,21 @@
-import { render, screen } from '@testing-library/react';
+import { getByTestId, render, screen } from '@testing-library/react';
 import App from './App';
+import FollowersFollowing from './pages/FollowersFollowing'
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+// test('renders learn react link', () => {
+//   render(<App />);
+//   const linkElement = screen.getByText(/learn react/i);
+//   expect(linkElement).toBeInTheDocument();
+// });
+describe("Test the App Component", () => {
+  test("header renders with react testing tutorial in the document", () => {
+    const component = render(<App />);
+    const linkElement = component.getByText(/Loading/i);
+    expect(linkElement).toBeInTheDocument();
+  });
+  test("render login component in doucment", () => {
+    const { getByTestId} = render(<App />);
+    const childElement = getByTestId("User ID:");
+    expect(childElement).toBeTruthy();
+  });
 });
